81b752aa25be1c72df78daff4b92066940cabf90
Raman <ramantiwary19@gmail.com>
2026-01-16 12:54:12 +0530

    Update resume with latest experince and education details

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1uPBn2_C2rDlgNQUH2JJv5PIADAiw1aVf/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1oOTMQzp-TMUvjR8Om0U53gWX2xmzn4Yo/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
